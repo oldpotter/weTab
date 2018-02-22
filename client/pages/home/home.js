a60b5e46784a7dc30236750a7064589a66695596
oldpotter <oldpottertom@icloud.com>
2018-02-22 10:31:47 +0800

    修改下页面

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -9,12 +9,22 @@ Page({
 	},
 
 	onClickSong(event){
+		console.log(event)
 		const index = event.currentTarget.dataset.index
 		wx.navigateTo({
 			url: `../edit/edit?index=${index}&songId=${app.songs[index].songId}`,
 			success: function (res) { },
 			fail: function (res) { },
 			complete: function (res) { },
+		})
+	},
+
+	onClickSearch(){
+		wx.navigateTo({
+			url: '../search/search',
+			success: function(res) {},
+			fail: function(res) {},
+			complete: function(res) {},
 		})
 	}
 })
